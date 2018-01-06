327f524b8a75642b6c36a868f4a91e62292edfaf
winsonyang <winsonyang@tencent.com>
2018-01-06 19:19:41 +0800

    anron_data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,6 +17,23 @@
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+enron_data = pickle.load(open("/Users/winson/PycharmProjects/ud120-projects/final_project/final_project_dataset.pkl", "r"))
 
+# print enron_data.items()
+# print enron_data.keys()
+print enron_data['METTS MARK'].keys()
+num_poi = 0
 
+for name in enron_data.keys():
+    if enron_data[name]["poi"] == 1:
+        num_poi += 1
+        print name
+
+print num_poi
+
+name = "Prentice James"
+print enron_data[name.upper()]["total_stock_value"]
+name = "Colwell Wesley"
+print enron_data[name.upper()]["from_this_person_to_poi"]
+name = "Skilling Jeffrey K"
+print enron_data[name.upper()]
